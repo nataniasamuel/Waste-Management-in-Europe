51b582db33226428e4fcae359f8879743687a310
nataniasamuel <nataniasamuel14@gmail.com>
2026-04-22 18:56:57 +0530

    created side with country selection box and year slider

---
+++ sidebar.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+#side bar function with europe as the default
+def sidebar(data):
+    st.sidebar.title("Dashboard Settings")
+    countries = sorted(data['Region'].unique().tolist())
+    default_idx = countries.index('European Union - 27 countries (from 2020)') if 'European Union - 27 countries (from 2020)' in countries else 0
+    country = st.sidebar.selectbox("Select a Country", options=countries, index = default_idx)
+
+#year slider
+    min_year = int(data['Year'].min())
+    max_year = int(data['Year'].max())
+    years = st.sidebar.slider("Drag to select a year range", min_year, max_year, (min_year, max_year))
+    return country, years
